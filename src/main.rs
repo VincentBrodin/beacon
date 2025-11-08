336e19bd5689188eadccf06de980698e1a2f8324
Vincent Brodin <vincent.brodin21@gmail.com>
2025-11-08 20:39:20 +0100

    setup iced

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{Column, button, column, text};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum Error {
+    #[error("Iced failed: {0}")]
+    Iced(#[from] iced::Error),
+}
+
+#[derive(Default)]
+struct Counter {
+    value: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    Increment,
+    Decrement,
+}
+
+impl Counter {
+    pub fn view(&'_ self) -> Column<'_, Message> {
+        column![
+            button("+").on_press(Message::Increment),
+            text(self.value).size(50),
+            button("-").on_press(Message::Decrement),
+        ]
+    }
+
+    pub fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), Error> {
+    match iced::run("beacon", Counter::update, Counter::view) {
+        Ok(()) => Ok(()),
+        Err(err) => Err(Error::Iced(err)),
+    }
 }
